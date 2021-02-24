6e9169c4a923203b3bfde9171d87e8c7b1ed3e8f
Giovanny Lucas Alves de Oliveira <giovannylucax@gmail.com>
2021-02-24 14:59:40 -0300

    feat: :sparkles: adicionada funcionalidade de depósito com validações
    adicionada rota para depósito, mudança no changeset para struct não obrigatoriamente vazio, função para soma dos valores depositados

--- lib/rocketpay_web/controllers/users_controller.ex
+++ lib/rocketpay_web/controllers/users_controller.ex
@@ -3,22 +3,21 @@ defmodule RocketpayWeb.UsersController do
 
   alias Rocketpay.User
 
+  action_fallback RocketpayWeb.FallbackController
+
   def create(connection, params) do
-    params
-    |> Rocketpay.create_user()
-    |> handle_response(connection)
+    with {:ok, %User{} = user} <- Rocketpay.create_user(params) do
+      connection
+      |> put_status(:created)
+      |> render("create.json", user: user)
+    end
   end
 
-  defp handle_response({:ok, %User{} = user}, connection) do
-    connection
-    |> put_status(:created)
-    |> render("create.json", user: user)
-  end
-
-  defp handle_response({:error, reason}, connection) do
-    connection
-    |> put_status(:bad_request)
-    |> put_view(RocketpayWeb.ErrorView)
-    |> render("400.json", result: reason)
-  end
+  defp handle_response({:error, _reason} = error, _connection), do: error
+  # defp handle_response({:error, reason}, connection) do
+  #   connection
+  #   |> put_status(:bad_request)
+  #   |> put_view(RocketpayWeb.ErrorView)
+  #   |> render("400.json", result: reason)
+  # end
 end
